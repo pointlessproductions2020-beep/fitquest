ec6c8baa07728961b67b71e61c01e8cbaa2bf3b4
pointlessproductions2020-beep <pointlessproductions2020@gmail.com>
2026-05-30 21:45:28 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,13 +1,15 @@
-// Your Firebase config here
+// --- Firebase Config ---
 const firebaseConfig = {
-    apiKey: "",
-    authDomain: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: ""
+  apiKey: "AIzaSyAVtxIzKrcj5VkTuSo7boaki3CfQzff3gA",
+  authDomain: "fitquest-1b9f1.firebaseapp.com",
+  projectId: "fitquest-1b9f1",
+  storageBucket: "fitquest-1b9f1.firebasestorage.app",
+  messagingSenderId: "958554033321",
+  appId: "1:958554033321:web:07b2fa75d1e4c9243e2db8",
+  measurementId: "G-ZVFR6222TF"
 };
 
+// --- Initialize Firebase ---
 firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
 
