b043fb516a02111521f37f22c4f947d99155d2a0
pointlessproductions2020-beep <pointlessproductions2020@gmail.com>
2026-06-02 16:50:06 +0100

    Fix storageBucket URL in firebase configuration
    Updated storageBucket to the correct Firebase bucket URL.

--- firebase.js
+++ firebase.js
@@ -3,7 +3,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyAVtxIzKrcj5VkTuSo7boaki3CfQzff3gA",
   authDomain: "fitquest-1b9f1.firebaseapp.com",
   projectId: "fitquest-1b9f1",
-  storageBucket: "fitquest-1b9f1f1.firebasestorage.app", // FIXED BUCKET
+  storageBucket: "fitquest-1b9f1.appspot.com",
   messagingSenderId: "958554033321",
   appId: "1:958554033321:web:07b2fa75d1e4c9243e2db8",
   measurementId: "G-ZVFR6222TF"
